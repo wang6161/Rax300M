0f901fadcc93d72ed62a09553d195eb7341f18ea
wang6161 <72291869+wang6161@users.noreply.github.com>
2025-10-04 10:53:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,6 @@
 #echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+#git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+#git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 
