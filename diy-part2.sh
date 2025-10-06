0bf5dd7dc5a8eb07fa13ad9423c23a764e92d33a
wang6161 <72291869+wang6161@users.noreply.github.com>
2025-10-06 22:23:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,9 @@
 #
 
 # Modify default IP
+# 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
+sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
